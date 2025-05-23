6f8164a59ec52f2068091fd397595b3ee8428de5
Laura <lsanchezmarabe@gmail.com>
2025-05-23 10:07:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,6 @@ def get_productos():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+   import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
